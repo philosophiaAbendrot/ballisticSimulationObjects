be6482935230ee42d2f318567067d6a352d1cd1b
Tony Ha <tonytaesung.ha@gmail.com>
2018-01-19 00:58:35 -0500

    Moved global variables to a global variable object.

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,32 @@
     $(document).ready(function(){
+      //global variable object
+      var globalVariables = {
+        frameInterval : 0.033, //30 fps
+        frameIntervalM : null,
+        pixelRatio : 0.2, //1px = 0.2m
+        index : 0,
+        xVel : 10,
+        yVel : 20,
+        xPos : 0,
+        yPos : 0
+      };
 
-      var frameInterval = 0.033; //30 fps
-      var frameIntervalM = frameInterval * 1000;
-      var pixelRatio = 0.2; //1px = 0.2m
-      var start = 0;
+      globalVariables.frameIntervalM = Math.round(globalVariables.frameInterval * 1000);
+
       var myVar;
-      var index = 0;
-      var xVel = 10;
-      var yVel = 20;
-      var xPos = 0;
-      var yPos = 0;
-
 
       //initialize telemetry panel
-      updatePanel(frameInterval,xPos,yPos,xVel,yVel);
+      updatePanel();
       $('button#startCommand').on('click', startFunction);
       $('button#stopCommand').on('click', stopFunction);
 
 
       function startFunction(){
-        xVel = Number($('input#velocityInput').val().split(',')[0]);
-        yVel = Number($('input#velocityInput').val().split(',')[1]);
-        xPos = Number($('input#initialPositionInput').val().split(',')[0]);
-        yPos = Number($('input#initialPositionInput').val().split(',')[1]);
-        myVar = setInterval(main,frameIntervalM);
+        globalVariables.xVel = Number($('input#velocityInput').val().split(',')[0]);
+        globalVariables.yVel = Number($('input#velocityInput').val().split(',')[1]);
+        globalVariables.xPos = Number($('input#initialPositionInput').val().split(',')[0]);
+        globalVariables.yPos = Number($('input#initialPositionInput').val().split(',')[1]);
+        myVar = setInterval(main,globalVariables.frameIntervalM);
       }
 
       function stopFunction(){
@@ -36,31 +39,31 @@
         var bottom;
         var left;
 
-        if (yPos < 0 && yVel < 0){
+        if (globalVariables.yPos < 0 && globalVariables.yVel < 0){
           return;
         }
 
-        index += 1;
-        xPos += xVel * frameInterval;
-        yPos += yVel * frameInterval;
-        yVel += yAcc * frameInterval;
-        xVel += xAcc * frameInterval;
-        bottom = Math.round(yPos / pixelRatio);
-        left = Math.round(xPos / pixelRatio);
+        globalVariables.index += 1;
+        globalVariables.xPos += globalVariables.xVel * globalVariables.frameInterval;
+        globalVariables.yPos += globalVariables.yVel * globalVariables.frameInterval;
+        globalVariables.yVel += yAcc * globalVariables.frameInterval;
+        globalVariables.xVel += xAcc * globalVariables.frameInterval;
+        bottom = Math.round(globalVariables.yPos / globalVariables.pixelRatio);
+        left = Math.round(globalVariables.xPos / globalVariables.pixelRatio);
 
-        updatePanel(frameInterval,xPos,yPos,xVel,yVel);
+        updatePanel();
         redraw(left,bottom);
 
       }
 
 
-      function updatePanel(frameInterval,xPos,yPos,xVel,yVel){
-        $('li:nth-child(1)').text("Frame Number: " + index);
-        $('li:nth-child(2)').text("T: : " + (index*frameInterval).toPrecision(3) + " s");
-        $('li:nth-child(3)').text("Height: " + yPos.toPrecision(3) + " m");
-        $('li:nth-child(4)').text("Down Range Distance: " + xPos.toPrecision(3) + " m");
-        $('li:nth-child(5)').text("Horizontal Velocity: " + xVel.toPrecision(3) + " m/s");
-        $('li:nth-child(6)').text("Vertical Velocity: " + yVel.toPrecision(3) + " m/s");
+      function updatePanel(){
+        $('li:nth-child(1)').text("Frame Number: " + globalVariables.index);
+        $('li:nth-child(2)').text("T: : " + (globalVariables.index*globalVariables.frameInterval).toPrecision(3) + " s");
+        $('li:nth-child(3)').text("Height: " + globalVariables.yPos.toPrecision(3) + " m");
+        $('li:nth-child(4)').text("Down Range Distance: " + globalVariables.xPos.toPrecision(3) + " m");
+        $('li:nth-child(5)').text("Horizontal Velocity: " + globalVariables.xVel.toPrecision(3) + " m/s");
+        $('li:nth-child(6)').text("Vertical Velocity: " + globalVariables.yVel.toPrecision(3) + " m/s");
       }
 
       function redraw(x,y){
