4a55c9e8623e0fb166907278b57c9473b178ba34
Tony Ha <tonytaesung.ha@gmail.com>
2018-01-19 13:50:41 -0500

    Fully objectified. Created physics object to contain global physics variables and panel object to create global panel variables.

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,39 @@
     $(document).ready(function(){
 
-      //global variable object
-      var globalVariables = {
-        frameInterval : 0.033, //30 fps
-        frameIntervalM : null,
-        pixelRatio : 0.2, //1px = 0.2m
-        index : 0,
-        xVel : 10,
-        yVel : 20,
-        xPos : 0,
-        yPos : 0
+      //physics object
+      var physics = {
+        variables: {
+          frameInterval : 0.033, //30 fps
+          frameIntervalM : null,
+          index : 0,
+          xVel : 10,
+          yVel : 20,
+          xPos : 0,
+          yPos : 0,
+          yAcc : -9.8,
+          xAcc : 0
+        },
+        crunch : function (){
+          this.variables.index += 1;
+          this.variables.xPos += this.variables.xVel * this.variables.frameInterval;
+          this.variables.yPos += this.variables.yVel * this.variables.frameInterval;
+          this.variables.yVel += this.variables.yAcc * this.variables.frameInterval;
+          this.variables.xVel += this.variables.xAcc * this.variables.frameInterval;
+        }
       };
 
-      globalVariables.frameIntervalM = Math.round(globalVariables.frameInterval * 1000);
+      physics.variables.frameIntervalM = Math.round(physics.variables.frameInterval * 1000);
 
       var myVar;
 
       //controller object
       var controller = {
         startFunction : function(){
-          globalVariables.xVel = Number($('input#velocityInput').val().split(',')[0]);
-          globalVariables.yVel = Number($('input#velocityInput').val().split(',')[1]);
-          globalVariables.xPos = Number($('input#initialPositionInput').val().split(',')[0]);
-          globalVariables.yPos = Number($('input#initialPositionInput').val().split(',')[1]);
-          myVar = setInterval(runtime.main,globalVariables.frameIntervalM);
+          physics.variables.xVel = Number($('input#velocityInput').val().split(',')[0]);
+          physics.variables.yVel = Number($('input#velocityInput').val().split(',')[1]);
+          physics.variables.xPos = Number($('input#initialPositionInput').val().split(',')[0]);
+          physics.variables.yPos = Number($('input#initialPositionInput').val().split(',')[1]);
+          myVar = setInterval(main,physics.variables.frameIntervalM);
         },
 
         stopFunction : function(){
@@ -33,60 +43,40 @@
 
       //panel object
       var panel = {
+        variables : {
+          bottom: 0,
+          left: 0,
+          pixelRatio : 0.2 //1px = 0.2m
+        },
+
         updatePanel : function(){
-          $('li:nth-child(1)').text("Frame Number: " + globalVariables.index);
-          $('li:nth-child(2)').text("T: : " + (globalVariables.index*globalVariables.frameInterval).toPrecision(3) + " s");
-          $('li:nth-child(3)').text("Height: " + globalVariables.yPos.toPrecision(3) + " m");
-          $('li:nth-child(4)').text("Down Range Distance: " + globalVariables.xPos.toPrecision(3) + " m");
-          $('li:nth-child(5)').text("Horizontal Velocity: " + globalVariables.xVel.toPrecision(3) + " m/s");
-          $('li:nth-child(6)').text("Vertical Velocity: " + globalVariables.yVel.toPrecision(3) + " m/s");
+          $('li:nth-child(1)').text("Frame Number: " + physics.variables.index);
+          $('li:nth-child(2)').text("T: : " + (physics.variables.index*physics.variables.frameInterval).toPrecision(3) + " s");
+          $('li:nth-child(3)').text("Height: " + physics.variables.yPos.toPrecision(3) + " m");
+          $('li:nth-child(4)').text("Down Range Distance: " + physics.variables.xPos.toPrecision(3) + " m");
+          $('li:nth-child(5)').text("Horizontal Velocity: " + physics.variables.xVel.toPrecision(3) + " m/s");
+          $('li:nth-child(6)').text("Vertical Velocity: " + physics.variables.yVel.toPrecision(3) + " m/s");
         },
 
-        redraw : function(x,y){
-          $('.sprite').css({'left': x, 'bottom':y});
+        redraw : function(){
+          this.bottom = Math.round(physics.variables.yPos / panel.variables.pixelRatio);
+          this.left = Math.round(physics.variables.xPos / panel.variables.pixelRatio);
+          $('.sprite').css({'left': this.left, 'bottom': this.bottom});
         }
       };
-
-      //physics object
-      var physics = {
-        variables: {
-          yAcc : -9.8,
-          xAcc : 0,
-        },
-
-        crunch : function (){
-          globalVariables.index += 1;
-          globalVariables.xPos += globalVariables.xVel * globalVariables.frameInterval;
-          globalVariables.yPos += globalVariables.yVel * globalVariables.frameInterval;
-          globalVariables.yVel += this.variables.yAcc * globalVariables.frameInterval;
-          globalVariables.xVel += this.variables.xAcc * globalVariables.frameInterval;
-        }
-      };
-
-
 
       //initialize telemetry panel
       panel.updatePanel();
       $('button#startCommand').on('click', controller.startFunction);
       $('button#stopCommand').on('click', controller.stopFunction);
 
-      var runtime = {
-        main : function (){
-
-          var bottom;
-          var left;
-
-          if (globalVariables.yPos < 0 && globalVariables.yVel < 0){
-            return;
-          }
-
-          physics.crunch();
-
-          bottom = Math.round(globalVariables.yPos / globalVariables.pixelRatio);
-          left = Math.round(globalVariables.xPos / globalVariables.pixelRatio);
-          panel.updatePanel();
-          panel.redraw(left,bottom);
+      var main = function (){
+        if (physics.variables.yPos < 0 && physics.variables.yVel < 0){
+          return;
         }
+        physics.crunch();
+        panel.updatePanel();
+        panel.redraw();
       };
 
     });
