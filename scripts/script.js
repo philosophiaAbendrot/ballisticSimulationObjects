2ff69b00ab3548c45a82e8dd634e354440bc0562
Tony Ha <tonytaesung.ha@gmail.com>
2018-01-19 12:25:16 -0500

    introduced better syntax for functions.

--- scripts/script.js
+++ scripts/script.js
@@ -15,25 +15,41 @@
 
       var myVar;
 
+      var startFunction = function(){
+        globalVariables.xVel = Number($('input#velocityInput').val().split(',')[0]);
+        globalVariables.yVel = Number($('input#velocityInput').val().split(',')[1]);
+        globalVariables.xPos = Number($('input#initialPositionInput').val().split(',')[0]);
+        globalVariables.yPos = Number($('input#initialPositionInput').val().split(',')[1]);
+        myVar = setInterval(main,globalVariables.frameIntervalM);
+      };
+
+      var stopFunction = function(){
+        clearInterval(myVar);
+      };
+
+      var updatePanel = function(){
+        $('li:nth-child(1)').text("Frame Number: " + globalVariables.index);
+        $('li:nth-child(2)').text("T: : " + (globalVariables.index*globalVariables.frameInterval).toPrecision(3) + " s");
+        $('li:nth-child(3)').text("Height: " + globalVariables.yPos.toPrecision(3) + " m");
+        $('li:nth-child(4)').text("Down Range Distance: " + globalVariables.xPos.toPrecision(3) + " m");
+        $('li:nth-child(5)').text("Horizontal Velocity: " + globalVariables.xVel.toPrecision(3) + " m/s");
+        $('li:nth-child(6)').text("Vertical Velocity: " + globalVariables.yVel.toPrecision(3) + " m/s");
+      };
+
+      var redraw = function(x,y){
+        $('.sprite').css({'left': x, 'bottom':y});
+      };
+
+
       //initialize telemetry panel
       updatePanel();
       $('button#startCommand').on('click', startFunction);
       $('button#stopCommand').on('click', stopFunction);
 
 
-      function startFunction(){
-        globalVariables.xVel = Number($('input#velocityInput').val().split(',')[0]);
-        globalVariables.yVel = Number($('input#velocityInput').val().split(',')[1]);
-        globalVariables.xPos = Number($('input#initialPositionInput').val().split(',')[0]);
-        globalVariables.yPos = Number($('input#initialPositionInput').val().split(',')[1]);
-        myVar = setInterval(main,globalVariables.frameIntervalM);
-      }
 
-      function stopFunction(){
-        clearInterval(myVar);
-      }
 
-      function main(){
+      var main = function (){
         var yAcc = -9.8;
         var xAcc = 0;
         var bottom;
@@ -54,19 +70,7 @@
         updatePanel();
         redraw(left,bottom);
 
-      }
+      };
 
 
-      function updatePanel(){
-        $('li:nth-child(1)').text("Frame Number: " + globalVariables.index);
-        $('li:nth-child(2)').text("T: : " + (globalVariables.index*globalVariables.frameInterval).toPrecision(3) + " s");
-        $('li:nth-child(3)').text("Height: " + globalVariables.yPos.toPrecision(3) + " m");
-        $('li:nth-child(4)').text("Down Range Distance: " + globalVariables.xPos.toPrecision(3) + " m");
-        $('li:nth-child(5)').text("Horizontal Velocity: " + globalVariables.xVel.toPrecision(3) + " m/s");
-        $('li:nth-child(6)').text("Vertical Velocity: " + globalVariables.yVel.toPrecision(3) + " m/s");
-      }
-
-      function redraw(x,y){
-        $('.sprite').css({'left': x, 'bottom':y});
-      }
     });
